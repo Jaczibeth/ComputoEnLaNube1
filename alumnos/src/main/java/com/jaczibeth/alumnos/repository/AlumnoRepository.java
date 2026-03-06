9ca71a5a0ea1a74fdbd245783b716d2331837ba7
Jaczibeth <jaczicruz@gmail.com>
2026-03-06 00:56:22 -0600

    cambios nuevos

--- alumnos/src/main/java/com/jaczibeth/alumnos/repository/AlumnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jaczibeth.alumnos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jaczibeth.alumnos.model.Alumno;
-
-public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
-
-}
